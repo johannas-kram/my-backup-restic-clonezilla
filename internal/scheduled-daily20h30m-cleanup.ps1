04e4890206e465d8d6ebc2e4e06bbfe070f3845b
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-03-16 17:11:31 +0100

    fixes

--- internal/scheduled-daily20h30m-cleanup.ps1
+++ internal/scheduled-daily20h30m-cleanup.ps1
@@ -1,6 +1,1 @@
 Start-Process "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe" "$Env:BACKUP_WORKING_DIR\internal\cleanup.ps1" -WindowStyle Minimized
-
-Write-Host ""
-Write-Host ""
-Write-Host "Press enter to close this window or close it manually"
-Read-Host ""
